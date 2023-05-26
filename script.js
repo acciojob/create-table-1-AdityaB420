b8f169f75e213b0f21a4594cf3e157dfb316c9c5
Aditya Butola <butola.aditya10@gmail.com>
2023-05-26 22:58:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 function insert_Row() {
-    //Write your code here
-	var table = document.getElementById("sampleTable");
+  var table = document.getElementById("sampleTable");
+  var newRow = table.insertRow(0); // Insert row at index 0 (top of the table)
 
-  // Insert a new row at the end of the table
-  var newRow = table.insertRow();
-
-  // Insert cells into the new row
   var cell1 = newRow.insertCell(0);
   var cell2 = newRow.insertCell(1);
 
-  // Set the cell values
-  cell1.innerHTML = "New Cell1";
-  cell2.innerHTML = "New Cell2";
-  
-  
+  cell1.innerHTML = "New Cell 1";
+  cell2.innerHTML = "New Cell 2";
 }
